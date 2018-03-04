65620d99d995c18d2c59c879d77336ae6897aa80
markmurimi <murimimg180@gmail.com>
2018-03-04 22:13:51 +0300

    add changes

--- js/scripts.js
+++ js/scripts.js
@@ -9,6 +9,61 @@ $(document).ready(function() {
     $('#display-players').show();
     $('#player1-Name').text(player1);
     $('#player2-Name').text(player2);
+    $('#score1').text (score1);
+    $('#roll1').click(player1.win1());
+    $('#score2').text(score2);
+    $('#roll2').click(player2.win2());
 
   });
 });
+
+// end of user interface
+
+// business logic
+
+var player1 = new Player();
+var player2 = new Player();
+
+var score1 = 0;
+var score2 = 0;
+
+Player.protoype.Win1 = function (){
+  var rolling1 = Math.floor(Math.random() * 6 + 1);
+
+  if (rolling1 == 1) {
+    alert("Next player.");
+    score1 = 0;
+  }else {
+    if (score1 >= 100) {
+      alert("Trophy goes to Player 1");
+    }else {
+      console.log('Continuing');
+      player1.points = rolling1;
+      score1 += rolling1;
+      console.log(score1);
+      console.log(rolling1 + score1);
+    }
+  }
+}
+
+player.prototype.Win2 = function (){
+  var rolling2 = Math.floor(Math.random() * 6 + 1);
+
+  if (rolling2 === 1){
+    alert("Next player.");
+    score2 = 0;
+  }else {
+    if (rolling2 >= 100){
+      alert("Trophy goes to player 2");
+    }else {
+      console.log('Continuing');
+      player2.points = rolling2;
+      score2 += rolling2;
+      console.log(score2);
+      console.log(rolling2 + score2);
+    }
+  }
+}
+
+console.log(player1);
+console.log(player2);
