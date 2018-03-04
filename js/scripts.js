f42e1740cf0ca375f9a3ef57e4ec7b137762c386
markmurimi <murimimg180@gmail.com>
2018-03-04 20:12:38 +0300

    modify various files

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,14 @@
+// user interface
 
+$(document).ready(function() {
+  $("form#inputForm").submit(function(event){
+    event.preventDefault();
+
+    var player1 = $('#player1-name').val();
+    var player2 = $('#player2-name').val();
+    $('#display-players').show();
+    $('#player1-Name').text(player1);
+    $('#player2-Name').text(player2);
+
+  });
+});
